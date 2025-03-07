26d04237aa8004f519d9dbe2e322cd09ccd2d3fe
Jully <150067528+juicejully@users.noreply.github.com>
2025-03-07 02:51:04 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
-function toggleDrawer() {
+document.addEventListener("click", (e) => {
   const drawer = document.querySelector(".drawer-content");
+  const button = document.querySelector(".drawer-button");
 
-  if (drawer.style.maxHeight) {
-    drawer.style.maxHeight = null; // Close
-  } else {
-    drawer.style.maxHeight = drawer.scrollHeight + "px"; // Open
+  // Toggle Drawer
+  if (e.target === button) {
+    drawer.classList.toggle("drawer-open");
+  } 
+  // Close Drawer on Outside Click
+  else if (!drawer.contains(e.target)) {
+    drawer.classList.remove("drawer-open");
   }
-}
+});
+
+// Close on button click
+document.querySelectorAll(".drawer-content button").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".drawer-content").classList.remove("drawer-open");
+  });
+});
